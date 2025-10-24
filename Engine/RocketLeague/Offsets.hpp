ed39dfedc270faf394c4dfc39a7159a2339a73ac
imattas <sup@zemi.gg>
2025-10-24 11:59:19 -0400

    Add Offsets.hpp with GNames and GObjects offsets

---
+++ Engine/RocketLeague/Offsets.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace Offsets {
+    inline constexpr uintptr_t GNames =   0x23d6d80;  //Update GNames Offset to the latest.
+    inline constexpr uintptr_t GObjects = 0x23d6dc8;  //Update GObjects Offset to the latest.
+}
